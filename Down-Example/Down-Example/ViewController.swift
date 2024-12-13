db04442fbe51ce7c85cd24fdf74c5f21758375f5
glt3953 <395313544@qq.com>
2024-12-13 11:08:08 +0800

    增加 html 渲染

--- Down-Example/Down-Example/ViewController.swift
+++ Down-Example/Down-Example/ViewController.swift
@@ -14,7 +14,84 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        renderDownInWebView()
+//        renderDownInWebView()
+//        let html = """
+//        <html>
+//        <body>
+//        <h1>Hello, world!</h1>
+//        </body>
+//        </html>
+//        """
+        let gridHtml = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                table {
+                    border-collapse: collapse;
+                    width: 100%;
+                    overflow: hidden;
+                }
+                th, td {
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    background-color: #fff;
+                    font-weight: normal; /* This makes sure the text is not bold */
+                }
+                th {
+                    width: 80;
+                    text-align: center;
+                }
+                td {
+                    text-align: left;
+                }
+                th:first-child {
+                    width: 100px;
+                }
+            </style>
+        </head>
+        <body>
+
+        <table>
+            <tr>
+                <th>人口</th>
+                <td>患有LPR和患有咽喉部疾病的对照组</td>
+            </tr>
+            <tr>
+                <th>样本量</th>
+                <td>162</td>
+            </tr>
+            <tr>
+                <th>方法</th>
+                <td>Systematic Review</td>
+            </tr>
+            <tr>
+                <th>结果</th>
+                <td>上皮化、后息肉复发、返流发现评分、返流症状指数</td>
+            </tr>
+            <tr>
+                <th>结论</th>
+                <td>PPI治疗改善了LPR患者的手术结果并减少复发</td>
+            </tr>
+        </table>
+
+        </body>
+        </html>
+        """
+        let gridTextView = UITextView(frame: CGRect(x: 50, y: 100, width: 200, height: 500))
+        gridTextView.isEditable = false
+        // 设置背景颜色
+        gridTextView.backgroundColor = UIColor.lightGray
+        // 设置圆角
+        gridTextView.layer.cornerRadius = 10
+        // 使圆角效果生效
+        gridTextView.layer.masksToBounds = true
+        if let attributedString = try? NSAttributedString(data: Data(gridHtml.utf8), options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+            gridTextView.attributedText = attributedString
+        }
+        view.addSubview(gridTextView)
     }
     
 }
